7694cf2be69e19914a78192d0b545c672b6aa392
lewjiayi <jiaareaper@hotmail.com>
2018-06-28 20:45:07 +0800

    updated message

--- Message.java
+++ Message.java
@@ -11,4 +11,46 @@ public class Message {
 	private String msgShotTRPot = ("A trap just revealed! Avoid it");
 	private String msgShotSRPot = ("Look! A ship just revealed. Destroy it!");
 	
+	public String msgWelcome() {
+		return msgWelcome;
+	}
+	
+	public String msgGameover() {
+		return msgGameover;
+	}
+	
+	public String msgShotOut() {
+		return msgShotOut;
+	}
+	
+	public String msgShotMiss() {
+		return msgShotMiss;
+	}
+	
+	public String msgShotShip() {
+		return msgShotShip;
+	}
+	
+	public String msgShotLDTrap() {
+		return msgShotLDTrap;
+	}
+	
+	public String msgShotHDTrap() {
+		return msgShotHDTrap;
+	}
+	
+	public String msgShotLSPot() {
+		return msgShotLSPot;
+	}
+	
+	public String msgShotTRPot() {
+		return msgShotTRPot;
+	}
+	
+	public String msgShotSRPot() {
+		return msgShotSRPot;
+	}
+	
+	
+	
 }
