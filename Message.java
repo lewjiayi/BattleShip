3f707d86b9e65155780ae6f45708812e8e5b904c
lewjiayi <jiaareaper@hotmail.com>
2018-06-28 17:56:07 +0800

    Added message file

---
+++ Message.java
@@ -0,0 +1,14 @@
+
+public class Message {
+	private String msgWelcome = ("Welcome to Battle Ship");
+	private String msgGameover = ("Game Over!");
+	private String msgShotOut = ("Coordinate Out of Bound");
+	private String msgShotMiss = ("Nothing is here...");
+	private String msgShotShip = ("Gotcha! You shot an enemy ship!");
+	private String msgShotLDTrap = ("It's a trap! You lose 1 life...");
+	private String msgShotHDTrap = ("Boom! A huge trap! You lose 2 life!");
+	private String msgShotLSPot = ("Life Potion! You gain 1 life!");
+	private String msgShotTRPot = ("A trap just revealed! Avoid it");
+	private String msgShotSRPot = ("Look! A ship just revealed. Destroy it!");
+	
+}
