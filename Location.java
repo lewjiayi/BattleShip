1e166c5c65d45e40ce90b454b20ec789c8bdfdb9
lewjiayi <jiaareaper@hotmail.com>
2018-06-29 14:01:57 +0800

    loc

--- Location.java
+++ Location.java
@@ -2,17 +2,25 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Location extends Rule {
-	private String [][] location = new String[getMapRow()][getMapCol()];
+	private int [][] location = new int[getMapRow()][getMapCol()];
 	private Boolean itemPlaced;
+	private int [][] shipLocation = new int[getShipNum()][2];
 	
 	public Location() {
-		for (String[]row : location) {
-			Arrays.fill(row, "0");
+		for (int[] row : location) {
+			Arrays.fill(row, 0);
 		}
 	}
 	
 	public void setShipLocation() {
-		
+		itemPlaced = false;
+		Random random = new Random();
+		for(int ship=0 ; ship<getShipNum()  ; ship++){
+			if(!itemPlaced) {
+				shipLocation[ship][0]=random.nextInt(5);
+				shipLocation[ship][1]=random.nextInt(5);
+			}
+		}
 	}
 	
 	
